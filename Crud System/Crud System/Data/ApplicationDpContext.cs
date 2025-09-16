4a03d53e346adb89adec60b23ea49cdb591966ea
etharani <tharani2297@gmail.com>
2025-09-16 16:37:17 +0530

    First Step for update project
    Crate system and create class student and ApplicationContext

---
+++ Crud System/Crud System/Data/ApplicationDpContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crud_System.Data
+{
+    public class ApplicationDpContext : DbContext
+    {
+        public ApplicationDpContext(DbContextOptions<ApplicationDpContext> options) :base(options)
+        {
+            
+        }
+    }
+}
---
+++ Crud System/Crud System/Models/Entities/student.cs
@@ -0,0 +1,12 @@
+﻿namespace Crud_System.Models.Entities
+{
+    public class student
+    {
+        public Guid StuId { get; set; }//Globally Unique Identifier
+        public required string StuName { get; set;}
+        public string StuAddress { get; set;}
+
+        public decimal? StuSalary { get; set;}
+        public int StuAge { get; set;}
+    }
+}
